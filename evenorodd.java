ebe00e3c1ba78e70514b27921a35b737918857ab
kalaiselviThiru <kkalaiselvi011325@gmail.com>
2024-12-07 12:27:54 +0530

    Update evenorodd.java

--- evenorodd.java
+++ evenorodd.java
@@ -6,12 +6,12 @@ public static void main(String args[])
 Scanner obj = new Scanner(System.in);
 System.out.print("enter a number:");
 int num = obj.nextInt();
-if(num%2 != 0)
-{
-System.out.print("it is odd number");
-}else
+if(num%2 == 0)
 {
 System.out.print("it is even number");
+}else
+{
+System.out.print("it is odd number");
 }
 }
 }
